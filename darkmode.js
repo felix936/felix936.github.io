9bde19305f988c632a097b570ea574a46d0998e6
Felix Burger <108411169+felix936@users.noreply.github.com>
2023-05-05 17:55:45 +0200

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,8 @@
 let darkMode = localStorage.getItem('darkMode');
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const darkModeToggle = document.querySelector('#darkmode-toggle');
 
 const enableDarkMode = () => {
+    document.getElementById("darkmode-toggle").checked = true;
     document.documentElement.style.setProperty('--foreground', 'var(--white)');
     document.documentElement.style.setProperty('--background', 'var(--black)');
     document.documentElement.style.setProperty('--lineargradient', 'linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))');
@@ -10,6 +11,7 @@ const enableDarkMode = () => {
     localStorage.setItem('darkMode', 'enabled');
 }
 const disableDarkMode = () => {
+    document.getElementById("darkmode-toggle").checked = false;
     document.documentElement.style.setProperty('--foreground', 'var(--black)');
     document.documentElement.style.setProperty('--background', 'var(--white)');
     document.documentElement.style.setProperty('--lineargradient', 'linear-gradient(rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0))');

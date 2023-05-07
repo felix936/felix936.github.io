31d1203cd2bb5acc3273e4a239bfad69c163e09d
Felix Burger <108411169+felix936@users.noreply.github.com>
2023-05-07 12:37:23 +0200

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -2,21 +2,21 @@ let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#darkmode-toggle');
 
 const enableDarkMode = () => {
-    document.getElementById("darkmode-toggle").checked = true;
     document.documentElement.style.setProperty('--foreground', 'var(--white)');
     document.documentElement.style.setProperty('--background', 'var(--black)');
     document.documentElement.style.setProperty('--lineargradient', 'linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0))');
     document.documentElement.style.setProperty('--articlebackground', 'var(--darkgrey)');
     document.documentElement.style.setProperty('--cvleftyearcolor', '#ab0e8e');
+    document.getElementById("darkmode-toggle").checked = true;
     localStorage.setItem('darkMode', 'enabled');
 }
 const disableDarkMode = () => {
-    document.getElementById("darkmode-toggle").checked = false;
     document.documentElement.style.setProperty('--foreground', 'var(--black)');
     document.documentElement.style.setProperty('--background', 'var(--white)');
     document.documentElement.style.setProperty('--lineargradient', 'linear-gradient(rgba(255, 255, 255, 1), rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0))');
     document.documentElement.style.setProperty('--articlebackground', 'var(--lightgrey)');
     document.documentElement.style.setProperty('--cvleftyearcolor', '#5c1f7d');
+    document.getElementById("darkmode-toggle").checked = false;
     localStorage.setItem('darkMode', 'null');
 }
 
@@ -31,10 +31,10 @@ darkModeToggle.addEventListener('click', () => {
     darkMode = localStorage.getItem('darkMode')
     if (darkMode !== 'enabled') {
         enableDarkMode();
-        console.log(darkMode);
+        console.log('Dark-Mode: ' + localStorage.getItem('darkMode'));
     }
     else {
         disableDarkMode();
-        console.log(darkMode);
+        console.log('Dark-Mode: ' + localStorage.getItem('darkMode'));
     }
 })
